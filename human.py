bfbeec3465718adbed001d461119676457a2e880
Prateek Garg <prateekgarg2208@gmail.com>
2020-09-02 01:42:06 +0530

    solo player

---
+++ human.py
@@ -0,0 +1,52 @@
+from classes import *
+
+def solo_play():
+    bird=Bird(230,350)
+    base=Base(630)
+    run =True
+    pipes=[Pipe(700)]
+    clock=pygame.time.Clock()
+    win=pygame.display.set_mode((WIN_WIDTH,WIN_HEIGHT))
+    score=0
+    draw_window(win,bird,pipes,base,score,1)
+    pygame.time.wait(1000)
+    while run:
+        clock.tick(30)
+        key_states = pygame.key.get_pressed()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run=False
+        if key_states[pygame.K_ESCAPE]:
+            run=False
+        if key_states[273]==1 or key_states[274]==1 or key_states[275]==1 or key_states[276]==1:
+            bird.jump()
+            # key_states[32]=0
+        rem=[]
+        for pipe in pipes:
+            if pipe.collide(bird):
+                run=False
+                break
+            if(pipe.x +pipe.PIPE_BOTTOM.get_width()<0):
+                rem.append(pipe)
+            add_pipe=False
+            if not pipe.passed and  pipe.x + pipe.PIPE_BOTTOM.get_width() < bird.x:
+                pipe.passed=True
+                add_pipe=True
+            pipe.move()
+            if add_pipe:
+                score+=1
+                pipes.append(Pipe(600))
+
+            for r in rem:
+                pipes.remove(r)
+        base.move()
+        bird.move()
+        draw_window(win,bird,pipes,base,score,1)
+        if bird.y+bird.img.get_height()>630 or bird.y<0:
+            run=False
+            break
+    pygame.time.wait(1000)
+    pygame.quit()
+    quit()
+if __name__=="__main__":
+    solo_play()
